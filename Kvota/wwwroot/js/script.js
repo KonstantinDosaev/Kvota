2e3a3a4b6ac855c7573653636e5a04957eb8575c
KonstantinDosaev <79165944+KonstantinDosaev@users.noreply.github.com>
2023-07-16 14:43:36 +0300

    Добавьте файлы проекта.

---
+++ Kvota/wwwroot/js/script.js
@@ -0,0 +1,12 @@
+
+//window.onBlazorReady = () => {
+//    $('.slider').slick({
+//        slidesToShow: 3,
+//        slidesToScroll: 1,
+//        autoplay: true,
+//        autoplaySpeed: 2000,
+//        arrows: false,
+//        rows: 3,
+//        adaptiveHeight: false
+//    });
+//}
